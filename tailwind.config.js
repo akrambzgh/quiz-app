95a0ee364a8eccb848d9c1b0455967c2d182b473
Akram Bouzoualegh <89752239+akrambzgh@users.noreply.github.com>
2023-05-11 16:23:48 +0100

    customizing the colours, upgrade the design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./app/public/index.html"],
   theme: {
+    colors: {
+      darkBlue: "#0C134F",
+      blue: "#1d267d",
+      darkPurple: "#5c469c",
+      lightPurple: "#d4adfc",
+    },
     extend: {},
   },
   plugins: [],
