9ac104530e046a83c2565946d2686aee005f1bdb
Akram Bouzoualegh <89752239+akrambzgh@users.noreply.github.com>
2023-05-28 00:00:07 +0100

    compile from ts to js, add comments to make the code better, fix the suggested answers inputs and texts problem

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,141 +1,112 @@
+"use strict";
 // Main Class
 class Quiz {
-  constructor(
-    questionInputElements,
-    questionTextElements,
-    suggestedInputElements,
-    addQuestionsSection,
-    suggestedTextElements,
-    answerQuestionsSection,
-    errorText,
-    allInputs
-  ) {
-    this.questionInputElements = questionInputElements;
-    this.questionTextElements = questionTextElements;
-    this.suggestedInputElements = suggestedInputElements;
-    this.suggestedTextElements = suggestedTextElements;
-    this.addQuestionsSection = addQuestionsSection;
-    this.answerQuestionsSection = answerQuestionsSection;
-    this.errorText = errorText;
-    this.allInputs = allInputs;
-  }
-
-  updateQuestions() {
-    let hasError = false;
-    allInputs.forEach((input) => {
-      if (input.value.trim() === "" || input.value.length <= 5) {
-        console.log(input);
-        hasError = true;
-        return;
-      }
-    });
-
-    if (hasError) {
-      errorText.classList.add("animation-to-right");
-      errorText.classList.remove("animation-to-left");
-    } else {
-      errorText.classList.add("animation-to-left");
-      errorText.classList.remove("animation-to-right");
+    constructor(questionInputElements, questionTextElements, suggestedInputElements, addQuestionsSection, suggestedTextElements, answerQuestionsSection, errorText, allInputs) {
+        this.questionInputElements = questionInputElements;
+        this.questionTextElements = questionTextElements;
+        this.suggestedInputElements = suggestedInputElements;
+        this.suggestedTextElements = suggestedTextElements;
+        this.addQuestionsSection = addQuestionsSection;
+        this.answerQuestionsSection = answerQuestionsSection;
+        this.errorText = errorText;
+        this.allInputs = allInputs;
     }
-
-    if (!hasError) {
-      this.questionTextElements.forEach((textElement, index) => {
-        textElement.textContent = questionInputElements[index].value;
-        console.log(textElement);
-      });
-      this.suggestedTextElements.forEach((textElement, index) => {
-        textElement.textContent = suggestedInputElements[index].value;
-        console.log(textElement);
-      });
-      this.addQuestionsSection.classList.add("hidden");
-      this.answerQuestionsSection.classList.remove("hidden");
+    // Method to update questions
+    updateQuestions() {
+        let hasError = false;
+        // Check for errors in input values
+        this.allInputs.forEach((input) => {
+            if (input.value.trim() === "" || input.value.length <= 5) {
+                hasError = true;
+                return;
+            }
+        });
+        // Show or hide error text based on error status
+        if (hasError) {
+            if (this.errorText) {
+                this.errorText.classList.add("show");
+            }
+        }
+        else {
+            if (this.errorText) {
+                this.errorText.classList.remove("show");
+            }
+        }
+        // Update question and suggested answer texts
+        if (!hasError) {
+            this.questionTextElements.forEach((textElement, index) => {
+                textElement.textContent = this.questionInputElements[index].value;
+            });
+            this.suggestedTextElements.forEach((textElement, index) => {
+                textElement.textContent = this.suggestedInputElements[index].value;
+            });
+            // Update question and suggested answer texts
+            if (this.addQuestionsSection) {
+                this.addQuestionsSection.classList.add("hidden");
+            }
+            if (this.answerQuestionsSection) {
+                this.answerQuestionsSection.classList.remove("hidden");
+            }
+        }
     }
-  }
 }
-
 class RadioButton {
-  constructor(radioElement) {
-    this.radioElement = radioElement;
-    this.sectionElement = radioElement.closest(".question-answer");
-    this.radioElement.addEventListener(
-      "click",
-      this.handleRadioClick.bind(this)
-    );
-  }
-
-  handleRadioClick() {
-    const section = this.sectionElement.getAttribute("data-section");
-
-    // Remove the "selected" class from all selected section elements
-    const sectionElements = document.querySelectorAll(
-      "questioning-sec .question-answer"
-    );
-
-    sectionElements.forEach((element) => {
-      element.classList.remove("selected");
-    });
-
-    if (this.radioElement.checked) {
-      // Add the "selected" class to the parent section
-      this.sectionElement.classList.add("selected");
-
-      // Store the input value in a variable or use it elsewhere
-      const selectedInput = this.sectionElement.querySelector(
-        ".suggested-answer-input"
-      );
-      const selectedInputValue = selectedInput.value;
-
-      console.log(
-        `This Is From ${section}, and This Is The Value: ${selectedInputValue}`
-      );
-    } else {
-      console.log(`Please select one input for ${section}`);
+    constructor(radioElement) {
+        this.radioElement = radioElement;
+        this.sectionElement = radioElement.closest(".question-answer");
+        // Add click event listener to radio button
+        this.radioElement.addEventListener("click", this.handleRadioClick.bind(this));
     }
-  }
+    // Handle radio button click event
+    handleRadioClick() {
+        if (this.sectionElement) {
+            const section = this.sectionElement.getAttribute("data-section");
+            // Remove the "selected" class from all selected section elements
+            const sectionElements = document.querySelectorAll(".questioning-sec .question-answer");
+            sectionElements.forEach((element) => {
+                element.classList.remove("selected");
+            });
+            if (this.radioElement.checked) {
+                // Add the "selected" class to the parent section
+                this.sectionElement.classList.add("selected");
+                // Store the input value in a variable or use it elsewhere
+                const selectedInput = this.sectionElement.querySelector(".suggested-answer-input");
+                const selectedInputValue = selectedInput === null || selectedInput === void 0 ? void 0 : selectedInput.value;
+                console.log(`This Is From ${section}, and This Is The Value: ${selectedInputValue}`);
+            }
+            else {
+                console.log(`Please select one input for ${section}`);
+            }
+        }
+    }
 }
-
-const radioButtons = document.querySelectorAll(
-  ".questioning-sec #radio-button"
-);
+// Get radio buttons and initialize RadioButton instances
+const radioButtons = document.querySelectorAll(".questioning-sec #radio-button");
 radioButtons.forEach((radioButton) => {
-  const radio = new RadioButton(radioButton);
+    const radio = new RadioButton(radioButton);
 });
-
 // Elements
 const questionInputElements = document.querySelectorAll(".question-input");
 const questionTextElements = document.querySelectorAll(".question-txt");
-const suggestedInputElements = document.querySelectorAll(
-  ".suggested-answer-input"
-);
-
-let addQuestionsSection = document.querySelector(".all-qs");
-let errorText = document.querySelector(".error");
-let allInputs = document.querySelectorAll('input[type="text"]');
-let suggestedTextElements = document.querySelectorAll(".suggetions");
-let answerQuestionsSection = document.querySelector(".all-answers");
-
-const quiz = new Quiz(
-  questionInputElements,
-  questionTextElements,
-  suggestedTextElements,
-  addQuestionsSection,
-  suggestedInputElements,
-  answerQuestionsSection,
-  radioButtons,
-  errorText,
-  allInputs
-);
-
+const suggestedTextElements = document.querySelectorAll(".suggetions");
+const suggestedInputElements = document.querySelectorAll(".suggested-answer-input");
+const addQuestionsSection = document.querySelector(".all-qs");
+const errorText = document.querySelector(".error");
+const allInputs = document.querySelectorAll('input[type="text"]');
+const answerQuestionsSection = document.querySelector(".all-answers");
+// Create Quiz instance
+const quiz = new Quiz(questionInputElements, questionTextElements, suggestedInputElements, addQuestionsSection, suggestedTextElements, answerQuestionsSection, errorText, allInputs);
+// Handle submit button click event
 const submitButton = document.querySelector(".confirm");
-submitButton.addEventListener("click", () => {
-  quiz.updateQuestions();
-  const selectedValues = [];
-  const sections = document.querySelectorAll(".question-answer.selected");
-  sections.forEach((section) => {
-    const selectedInput = section.querySelector(".suggested-answer-input");
-    if (selectedInput) {
-      selectedValues.push(selectedInput.value);
-    }
-  });
-  // console.log("Selected Input Values:", selectedValues);
+submitButton === null || submitButton === void 0 ? void 0 : submitButton.addEventListener("click", () => {
+    quiz.updateQuestions();
+    // Get selected values
+    const selectedValues = [];
+    const sections = document.querySelectorAll(".question-answer.selected");
+    sections.forEach((section) => {
+        const selectedInput = section.querySelector(".suggested-answer-input");
+        if (selectedInput) {
+            selectedValues.push(selectedInput.value);
+        }
+    });
 });
--- app/src/ts/app.ts
+++ app/src/ts/app.ts
@@ -1,15 +1,184 @@
-// class AddQuestions {
-//   constructor(
-//     allQuestionsInputs:NodeListOf<HTMLInputElement[]>,
-//     allSuggesedAnswers: NodeListOf<HTMLElement[]>
-//   ) {}
-//   showMe() {
-//     console.log(allQuestionsInputs, allSuggesedAnswers);
-//   }
-// }
-// let allQuestionsInputs = document.querySelectorAll(".question-input");
-// let allSuggesedAnswers = document.querySelectorAll(".question-answer");
+// Main Class
+class Quiz {
+  // Properties
+  public questionInputElements: NodeListOf<HTMLInputElement>;
+  public questionTextElements: NodeListOf<HTMLElement>;
+  public suggestedInputElements: NodeListOf<HTMLInputElement>;
+  public suggestedTextElements: NodeListOf<HTMLElement>;
+  public addQuestionsSection: HTMLElement | null;
+  public answerQuestionsSection: HTMLElement | null;
+  public errorText: HTMLElement | null;
+  public allInputs: NodeListOf<HTMLInputElement>;
 
-// const addQuestions = new AddQuestions(allQuestionsInputs, allSuggesedAnswers);
+  constructor(
+    questionInputElements: NodeListOf<HTMLInputElement>,
+    questionTextElements: NodeListOf<HTMLElement>,
+    suggestedInputElements: NodeListOf<HTMLInputElement>,
+    addQuestionsSection: HTMLElement | null,
+    suggestedTextElements: NodeListOf<HTMLElement>,
+    answerQuestionsSection: HTMLElement | null,
+    errorText: HTMLElement | null,
+    allInputs: NodeListOf<HTMLInputElement>
+  ) {
+    this.questionInputElements = questionInputElements;
+    this.questionTextElements = questionTextElements;
+    this.suggestedInputElements = suggestedInputElements;
+    this.suggestedTextElements = suggestedTextElements;
+    this.addQuestionsSection = addQuestionsSection;
+    this.answerQuestionsSection = answerQuestionsSection;
+    this.errorText = errorText;
+    this.allInputs = allInputs;
+  }
 
-// console.log(addQuestions.showMe());
+  // Method to update questions
+  updateQuestions() {
+    let hasError = false;
+
+    // Check for errors in input values
+    this.allInputs.forEach((input) => {
+      if (input.value.trim() === "" || input.value.length <= 5) {
+        hasError = true;
+        return;
+      }
+    });
+
+    // Show or hide error text based on error status
+    if (hasError) {
+      if (this.errorText) {
+        this.errorText.classList.add("show");
+      }
+    } else {
+      if (this.errorText) {
+        this.errorText.classList.remove("show");
+      }
+    }
+
+    // Update question and suggested answer texts
+    if (!hasError) {
+      this.questionTextElements.forEach((textElement, index) => {
+        textElement.textContent = this.questionInputElements[index].value;
+      });
+      this.suggestedTextElements.forEach((textElement, index) => {
+        textElement.textContent = this.suggestedInputElements[index].value;
+      });
+
+      // Update question and suggested answer texts
+      if (this.addQuestionsSection) {
+        this.addQuestionsSection.classList.add("hidden");
+      }
+      if (this.answerQuestionsSection) {
+        this.answerQuestionsSection.classList.remove("hidden");
+      }
+    }
+  }
+}
+
+class RadioButton {
+  // Properties
+  public radioElement: HTMLInputElement;
+  public sectionElement: HTMLElement | null;
+
+  constructor(radioElement: HTMLInputElement) {
+    this.radioElement = radioElement;
+    this.sectionElement = radioElement.closest(".question-answer");
+
+    // Add click event listener to radio button
+    this.radioElement.addEventListener(
+      "click",
+      this.handleRadioClick.bind(this)
+    );
+  }
+
+  // Handle radio button click event
+  handleRadioClick() {
+    if (this.sectionElement) {
+      const section = this.sectionElement.getAttribute("data-section");
+
+      // Remove the "selected" class from all selected section elements
+      const sectionElements = document.querySelectorAll(
+        ".questioning-sec .question-answer"
+      );
+
+      sectionElements.forEach((element) => {
+        element.classList.remove("selected");
+      });
+
+      if (this.radioElement.checked) {
+        // Add the "selected" class to the parent section
+        this.sectionElement.classList.add("selected");
+
+        // Store the input value in a variable or use it elsewhere
+        const selectedInput =
+          this.sectionElement.querySelector<HTMLInputElement>(
+            ".suggested-answer-input"
+          );
+        const selectedInputValue = selectedInput?.value;
+
+        console.log(
+          `This Is From ${section}, and This Is The Value: ${selectedInputValue}`
+        );
+      } else {
+        console.log(`Please select one input for ${section}`);
+      }
+    }
+  }
+}
+
+// Get radio buttons and initialize RadioButton instances
+const radioButtons = document.querySelectorAll<HTMLInputElement>(
+  ".questioning-sec #radio-button"
+);
+radioButtons.forEach((radioButton) => {
+  const radio = new RadioButton(radioButton);
+});
+
+// Elements
+const questionInputElements =
+  document.querySelectorAll<HTMLInputElement>(".question-input");
+const questionTextElements =
+  document.querySelectorAll<HTMLElement>(".question-txt");
+
+const suggestedTextElements =
+  document.querySelectorAll<HTMLElement>(".suggetions");
+const suggestedInputElements = document.querySelectorAll<HTMLInputElement>(
+  ".suggested-answer-input"
+);
+
+const addQuestionsSection = document.querySelector<HTMLElement>(".all-qs");
+const errorText = document.querySelector<HTMLElement>(".error");
+const allInputs =
+  document.querySelectorAll<HTMLInputElement>('input[type="text"]');
+const answerQuestionsSection =
+  document.querySelector<HTMLElement>(".all-answers");
+
+// Create Quiz instance
+const quiz = new Quiz(
+  questionInputElements,
+  questionTextElements,
+  suggestedInputElements,
+  addQuestionsSection,
+  suggestedTextElements,
+  answerQuestionsSection,
+  errorText,
+  allInputs
+);
+
+// Handle submit button click event
+const submitButton = document.querySelector<HTMLButtonElement>(".confirm");
+submitButton?.addEventListener("click", () => {
+  quiz.updateQuestions();
+
+  // Get selected values
+  const selectedValues: string[] = [];
+  const sections = document.querySelectorAll<HTMLElement>(
+    ".question-answer.selected"
+  );
+  sections.forEach((section) => {
+    const selectedInput = section.querySelector<HTMLInputElement>(
+      ".suggested-answer-input"
+    );
+    if (selectedInput) {
+      selectedValues.push(selectedInput.value);
+    }
+  });
+});
