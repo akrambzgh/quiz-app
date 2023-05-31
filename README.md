6990e80542597143ea36c38d98cbbc2a56248ef6
Akram Bouzoualegh <89752239+akrambzgh@users.noreply.github.com>
2023-05-31 20:02:07 +0100

    Add Readme.MD file, change the design, make the code better

---
+++ README.md
@@ -0,0 +1,57 @@
+# Quiz Website
+
+Quiz Website is a web application that allows users to create and participate in quizzes. Users can add questions, provide suggested answers, and test their knowledge by selecting the correct answers.
+
+## Features
+
+- Add questions and suggested answers
+- Select the correct answer for each question
+- Submit the quiz and check the selected answers
+- Receive feedback on right and wrong answers
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Getting Started
+
+To get a local copy of the project up and running, follow these steps:
+
+1. Clone the repository: https://github.com/akrambzgh/quiz-app
+
+2. Open the project directory: cd quiz-app
+
+3. Open the `index.html` file in your web browser.
+
+## Usage
+
+1. Add questions and suggested answers:
+
+- Enter the question in the "Question" input field.
+- Enter four suggested answers in the "Suggested Answer" input fields.
+- Click the "Add Question" button to add the question and suggested answers to the quiz.
+
+2. Select the correct answer:
+
+- Click on the suggested answer that you believe is the correct one for each question.
+- The selected answer will be highlighted.
+
+3. Submit the quiz:
+
+- Click the "Submit" button to submit your answers.
+- A message will appear indicating whether each answer is right or wrong.
+
+4. Review the quiz:
+
+- You can review the quiz by scrolling through the questions and answers.
+- Right answers will be marked with a special indicator.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvement, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,5 +1,5 @@
 "use strict";
-// Main Class
+
 class Quiz {
   constructor(
     questionInputElements,
@@ -20,80 +20,67 @@ class Quiz {
     this.errorText = errorText;
     this.allInputs = allInputs;
   }
-  // Method to update questions
+
   updateQuestions() {
     let hasError = false;
-    // Check for errors in input values
+
     this.allInputs.forEach((input) => {
       if (input.value.trim() === "" || input.value.length <= 5) {
         hasError = true;
         return;
       }
     });
-    // Show or hide error text based on error status
+
     if (hasError) {
-      if (this.errorText) {
-        this.errorText.classList.add("show");
-      }
+      this.errorText?.classList.add("show");
     } else {
-      if (this.errorText) {
-        this.errorText.classList.remove("show");
-      }
+      this.errorText?.classList.remove("show");
     }
-    // Update question and suggested answer texts
+
     if (!hasError) {
       this.questionTextElements.forEach((textElement, index) => {
         textElement.textContent = this.questionInputElements[index].value;
       });
+
       this.suggestedTextElements.forEach((textElement, index) => {
         textElement.textContent = this.suggestedInputElements[index].value;
       });
-      // Update question and suggested answer texts
-      if (this.addQuestionsSection) {
-        this.addQuestionsSection.classList.add("hidden");
-      }
-      if (this.answerQuestionsSection) {
-        this.answerQuestionsSection.classList.remove("hidden");
-      }
+
+      this.addQuestionsSection?.classList.add("hidden");
+      this.answerQuestionsSection?.classList.remove("hidden");
     }
   }
 }
 
-class GetAnswerValue {
-  constructor(sectionElement) {
-    this.sectionElement = sectionElement;
-    this.sectionElement.addEventListener("click", () => {
-      this.handleRadioClickOne();
-    });
-  }
-  // Handle radio button click event
-  handleRadioClickOne() {
-    if (this.sectionElement) {
-      const section = this.sectionElement.getAttribute("data-section");
-      if (this.sectionElement) {
-        // Store the input value in a variable or use it elsewhere
-        const selectedInput = this.sectionElement.querySelector(
-          ".suggested-answer-input"
-        );
-        const selectedInputVlaue = selectedInput.value;
-        console.log(selectedInputVlaue);
-      } else {
-        console.log(`Please select one input for ${section}`);
-      }
-    }
-  }
-}
-const answerSections = document.querySelectorAll(
-  ".questioning-sec .question-answer"
-);
-answerSections.forEach((section) => {
-  const answer = new GetAnswerValue(section);
-});
+// class GetAnswerValue {
+//   constructor(sectionElement) {
+//     this.sectionElement = sectionElement;
+//     this.sectionElement.addEventListener("click", () => {
+//       this.handleRadioClickOne();
+//     });
+//   }
+
+//   handleRadioClickOne() {
+//     const section = this.sectionElement.getAttribute("data-section");
+
+//     if (this.sectionElement) {
+//       const selectedInput = this.sectionElement.querySelector(
+//         ".suggested-answer-input"
+//       );
+
+//       if (selectedInput) {
+//         const selectedInputValue = selectedInput.value;
+//         console.log(selectedInputValue);
+//       } else {
+//         console.log(`Please select one input for ${section}`);
+//       }
+//     }
+//   }
+// }
 
 class SelectRightAnswer {
-  constructor(answerSections, score) {
+  constructor(answerSections) {
     this.answerSections = answerSections;
-    this.score = score;
     this.answerSections.addEventListener("click", () => {
       this.checkRightAnswer();
     });
@@ -101,43 +88,98 @@ class SelectRightAnswer {
 
   checkRightAnswer() {
     if (this.answerSections.getAttribute("data-right-wrong") === "right") {
+      // Correct answer
+      this.answerSections.classList.add("selected-right");
     } else {
-      console.log("Worng Answer");
+      // Wrong answer
+      this.answerSections.classList.add("selected-wrong");
     }
   }
 }
 
-// Define a class for each section
 class QuestionSection {
-  constructor(sections) {
-    this.sections = sections; // Array to store text elements
-  }
-
-  handleClick(index) {
-    // Reset color of all text elements in the section to default
-    this.sections.forEach((section) => {
-      section.dataset.rightWrong = "wrong";
-      section.classList.remove("right-answer");
-    });
-
-    // Set color of the clicked text element to red
-    this.sections[index].classList.add("right-answer");
-    this.sections[index].dataset.rightWrong = "right";
-  }
-}
-class AnswerSection {
   constructor(sections) {
     this.sections = sections;
   }
 
   handleClick(index) {
     this.sections.forEach((section) => {
-      section.classList.remove("selected");
+      section.dataset.rightWrong = "wrong";
+      section.classList.remove("right-answer");
     });
 
-    this.sections[index].classList.add("selected");
+    this.sections[index].classList.add("right-answer");
+    this.sections[index].dataset.rightWrong = "right";
   }
 }
+
+// class AnswerSection {
+//   constructor(sections) {
+//     this.sections = sections;
+//   }
+
+//   handleClick(index) {
+//     this.sections.forEach((section) => {
+//       section.classList.remove("selected-right");
+//       section.classList.remove("selected-wrong");
+//     });
+
+//     section.classList.add("selected-right");
+//     section.classList.add("selected-wrong");
+//   }
+// }
+
+const questionInputElements = document.querySelectorAll(".question-input");
+const questionTextElements = document.querySelectorAll(".question-txt");
+const suggestedTextElements = document.querySelectorAll(".suggetions");
+const suggestedInputElements = document.querySelectorAll(
+  ".suggested-answer-input"
+);
+const addQuestionsSection = document.querySelector(".all-qs");
+const errorText = document.querySelector(".error");
+const allInputs = document.querySelectorAll('input[type="text"]');
+const answerQuestionsSection = document.querySelector(".all-answers");
+const allSuggestedTextSections = document.querySelectorAll(".answer");
+const allQuestionSections = document.querySelectorAll(".question-answer");
+
+const quiz = new Quiz(
+  questionInputElements,
+  questionTextElements,
+  suggestedInputElements,
+  addQuestionsSection,
+  suggestedTextElements,
+  answerQuestionsSection,
+  errorText,
+  allInputs
+);
+
+const submitButton = document.querySelector(".confirm");
+submitButton?.addEventListener("click", () => {
+  const selectedValues = [];
+  const sections = document.querySelectorAll(".question-answer.right-answer");
+
+  // if (sections.length < 10) {
+  //   alert("You have to select at least 10 right answers");
+  // } else {
+  quiz.updateQuestions();
+
+  sections.forEach((section) => {
+    const selectedInput = section.querySelector(".suggested-answer-input");
+
+    if (selectedInput) {
+      selectedValues.push(selectedInput.value);
+    }
+  });
+
+  allSuggestedTextSections.forEach((section, index) => {
+    section.dataset.rightWrong = allQuestionSections[index].dataset.rightWrong;
+  });
+  // }
+});
+
+allSuggestedTextSections.forEach((section) => {
+  const rightAnswer = new SelectRightAnswer(section);
+});
 
 // Create section objects
 const questionSectionsArray = [];
@@ -158,81 +200,32 @@ for (let i = 0; i < 10; i++) {
 
   questionSectionsArray.push(questionSection);
 }
-// Create section objects
-const answersectionsArray = [];
 
-// Add text elements to each section
-for (let i = 0; i < 10; i++) {
-  const answerSectionsElements = document.querySelectorAll(
-    `.answering${i} .answer`
-  );
+// // Create section objects
+// const answersectionsArray = [];
 
-  const answerSection = new AnswerSection(answerSectionsElements);
+// // Add text elements to each section
+// for (let i = 0; i < 10; i++) {
+//   const answerSectionsElements = document.querySelectorAll(
+//     `.answering${i} .answer`
+//   );
 
-  answerSectionsElements.forEach((answerSec, index) => {
-    answerSec.addEventListener("click", () => {
-      answerSection.handleClick(index);
-    });
-  });
+//   const answerSection = new AnswerSection(answerSectionsElements);
 
-  answersectionsArray.push(answerSection);
-}
+//   answerSectionsElements.forEach((answerSec, index) => {
+//     answerSec.addEventListener("click", () => {
+//       answerSection.handleClick(index);
+//     });
+//   });
 
-// Elements
-const questionInputElements = document.querySelectorAll(".question-input");
-const questionTextElements = document.querySelectorAll(".question-txt");
-const suggestedTextElements = document.querySelectorAll(".suggetions");
-const suggestedInputElements = document.querySelectorAll(
-  ".suggested-answer-input"
+//   answersectionsArray.push(answerSection);
+// }
+
+// Get answer sections and initialize GetAnswerValue for each section
+const answerSections = document.querySelectorAll(
+  ".questioning-sec .question-answer"
 );
-const addQuestionsSection = document.querySelector(".all-qs");
-const errorText = document.querySelector(".error");
-const allInputs = document.querySelectorAll('input[type="text"]');
-const answerQuestionsSection = document.querySelector(".all-answers");
-let allSuggestedTextSections = document.querySelectorAll(".answer");
-let allQuestionSections = document.querySelectorAll(".question-answer");
 
-// Create Quiz instance
-const quiz = new Quiz(
-  questionInputElements,
-  questionTextElements,
-  suggestedInputElements,
-  addQuestionsSection,
-  suggestedTextElements,
-  answerQuestionsSection,
-  errorText,
-  allInputs
-);
-// Handle submit button click event
-const submitButton = document.querySelector(".confirm");
-submitButton === null || submitButton === void 0
-  ? void 0
-  : submitButton.addEventListener("click", () => {
-      // Get selected values
-      const selectedValues = [];
-      const sections = document.querySelectorAll(
-        ".question-answer.right-answer"
-      );
-      // if (sections.length < 10) {
-      // alert("You Have To Select At Least 10 Right Answers");
-      // } else {
-      quiz.updateQuestions();
-      sections.forEach((section) => {
-        const selectedInput = section.querySelector(".suggested-answer-input");
-        if (selectedInput) {
-          selectedValues.push(selectedInput.value);
-        }
-      });
-
-      allSuggestedTextSections.forEach((section, index) => {
-        section.dataset.rightWrong =
-          allQuestionSections[index].dataset.rightWrong;
-      });
-      // }
-    });
-
-let score = 0;
-
-allSuggestedTextSections.forEach((section) => {
-  const rigthAnswer = new SelectRightAnswer(section);
-});
+// answerSections.forEach((section) => {
+//   const answer = new GetAnswerValue(section);
+// });
